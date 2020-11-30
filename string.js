65951fcdae31c34a4d5a50c3b845f42daf72aa81
George_Thomas <George_Thomas@student.mahoningctc.com>
2020-11-30 14:21:10 -0500

    ysfgfgd

--- string.js
+++ string.js
@@ -1,19 +1,21 @@
 // alert("Hello Ninja");
 
-var x = 5;
-var y = 10;
-var xSquared = Math.pow(x, 2);
-var ySquared = Math.pow(y, 2);
-var z = Math.sqrt(xSquared + ySquared);
-var name = "Ninja";
-console.log("Your name is " + name);
-console.log("x + y = " + x + y);
-console.log("x + y = " + parseInt(x + y));
-console.log("x + y = " + "Ninja");
+// var x = 5;
+// var y = 10;
+// var xSquared = Math.pow(x, 2);
+// var ySquared = Math.pow(y, 2);
+// var z = Math.sqrt(xSquared + ySquared);
+// var name = "Ninja";
+// console.log("Your name is " + name);
+// console.log("x + y = " + x + y);
+// console.log("x + y = " + parseInt(x + y));
+// console.log("x + y = " + "Ninja");
+
+// alert("Hello, world")
 
 alert("Hello, what is your name?")
 name = prompt("Please input your name: ")
-alert("Welcome " + name + ". It is a great to see you today.")
+alert("Welcome " + name + ". It is great to see you today.")
 console.log("Your name is now: " + name);
 console.log('Your name is now: ' + name);
 console.log(`Your name is now: ${name} `);
