c81fbb65b3a96ef20a90bf17461ddb8aefdf1c76
George_Thomas <George_Thomas@student.mahoningctc.com>
2020-12-02 10:48:40 -0500

    End of Day 2

---
+++ Day 2/variables.js
@@ -0,0 +1,29 @@
+let a;
+a = 5;
+let b = 10;
+answer = b+a;
+console.log(answer)
+// document.writeIn(a+b)
+
+let td = 6;
+let fg = 1;
+let safety = 2;
+let extraPoint = 2;
+
+var score = 0;
+
+score = score + td;
+score += safety;
+score += safety;
+score += safety;
+score += safety;
+score += safety;
+score += safety;
+score += safety;
+// To do multiple select: Shift + Ctrl + Alt up or down arrow Key.
+// Or another option is to press and hold the scroll key on your mouse
+
+document.getElementById("theAnswer").innerHTML = score;
+
+var myString = 'It doesn\'t look like it\'s walking weather.'
+document.getElementById("paragraph").innerHTML = myString.split(" ");
